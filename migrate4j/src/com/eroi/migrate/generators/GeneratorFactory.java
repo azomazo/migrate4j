f9868e232de94639fd80c857cbce943dc487f347
sam_ware <sam_ware@56aca16a-3d3c-0410-8b25-e62c3009fa6b>
2008-01-04 09:31:39 +0000

    Logging Added

--- migrate4j/src/com/eroi/migrate/generators/GeneratorFactory.java
+++ migrate4j/src/com/eroi/migrate/generators/GeneratorFactory.java
@@ -2,11 +2,14 @@ package com.eroi.migrate.generators;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
+import com.eroi.migrate.Configure;
 import com.eroi.migrate.misc.SchemaMigrationException;
 
 public class GeneratorFactory {
-
+	private static Log log = LogFactory.getLog(GeneratorFactory.class);
     public static Generator getGenerator(Connection connection) throws SQLException {
         String dbName = connection.getMetaData().getDatabaseProductName();
 
@@ -17,6 +20,7 @@ public class GeneratorFactory {
         } else if ("MySQL".equalsIgnoreCase(dbName)) {
             return new MySQLGenerator();
         } else {
+             log.error("No DDLGenerator found for \"" + dbName + "\".  You may need to write your own!");
             throw new SchemaMigrationException("No DDLGenerator found for \"" + dbName + "\".  You may need to write your own!");
         }
 
